689b594ffeb04d04aeaa760aadb19cc160a125b8
toliuyi <liuyi3@gmail.com>
2018-04-12 09:35:13 +0800

    translation update

--- ui/app/welcome-screen.js
+++ ui/app/welcome-screen.js
@@ -33,7 +33,7 @@ class WelcomeScreen extends Component {
 
           h('div.welcome-screen__info__header', '欢迎使用以太狗'),
 
-          h('div.welcome-screen__info__copy', '以太狗是简洁易用的以太坊浏览器钱包，是基于Metamask开发的开源浏览器插件，是Metamask的中文精简版。您可以用以太狗管理以太币（ETH）和基于以太坊的代币(Token)，并操作以太坊分布式应用（Dapp)。'),
+          h('div.welcome-screen__info__copy', '以太狗是简洁易用的以太坊浏览器钱包，是基于Metamask开发的开源浏览器插件，是Metamask的中文精简版。您可以用以太狗管理以太币（ETH）和基于以太坊的代币(Token)，并操作以太坊去中心化应用（Dapp)。'),
 
           h('button.welcome-screen__button', {
             onClick: this.initiateAccountCreation,
